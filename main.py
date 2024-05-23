fa1dff7c4ea8b7898125dba22c4db1ffd123cc53
Adil S <forever@nzironeed.ru>
2024-05-23 22:50:59 +0500

    added validator

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from copy import deepcopy
 
 
+def validate(banknotes=None, amount=None):
+    maximum = 10 ** 9
+
+    if banknotes:
+
+        if len(banknotes) != 5:
+            raise Exception('Error: banknotesCount length must be 5')
+
+        for banknote in banknotes:
+            if banknote < 0 or banknote > maximum:
+                raise Exception('Error: Banknote value must be between 0 and 10^9')
+
+        return banknotes
+
+    if amount and amount < 0 or (amount > maximum):
+        raise Exception('Error: Amount must be at least 1')
+
+    return amount
+
+
 class ATM(object):
 
     def __init__(self):
@@ -11,6 +31,7 @@ class ATM(object):
         :type banknotesCount: List[int]
         :rtype: None
         """
+        banknotesCount = validate(banknotes=banknotesCount)
         for i, banknote in enumerate(self.cash):
             self.cash[banknote] += banknotesCount[i]
 
@@ -20,6 +41,7 @@ class ATM(object):
         :rtype: List[int]
         """
         res, cash = [], deepcopy(self.cash)
+        amount = validate(amount=amount)
 
         for banknote in list(cash.keys())[::-1]:
             banknote_count = amount // banknote
@@ -39,9 +61,10 @@ class ATM(object):
         return res[::-1]
 
 
-atm = ATM()
-atm.deposit([0,0,1,2,1])
-print(atm.withdraw(600)) # [0,0,1,0,1]
-atm.deposit([0,1,0,1,1])
-print(atm.withdraw(600)) # [-1]
-print(atm.withdraw(550)) # [0,1,0,0,1]
+if __name__ == '__main__':
+    atm = ATM()
+    atm.deposit([0,0,1,2,1])
+    print(atm.withdraw(600)) # [0,0,1,0,1]
+    atm.deposit([0,1,0,1,1])
+    print(atm.withdraw(600)) # [-1]
+    print(atm.withdraw(550)) # [0,1,0,0,1]
